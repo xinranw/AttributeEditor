c1addcddea0b1feac7749061fd1f2db598653735
Xinran Wang <xw1992@gmail.com>
2013-09-29 15:37:29 -0400

    Initial Commit

---
+++ AttributeEditor/XIWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XIWAppDelegate.h
+//  AttributeEditor
+//
+//  Created by User on 9/29/13.
+//  Copyright (c) 2013 Xinran Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XIWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AttributeEditor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AttributeEditor
+//
+//  Created by User on 9/29/13.
+//  Copyright (c) 2013 Xinran Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XIWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XIWAppDelegate class]));
+    }
+}
